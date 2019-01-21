2425baba9ecfd6668c674cdf01164378185cca1c
Thế Nguyên Phạm <nguyendptse62666@fpt.edu.vn>
2019-01-21 17:04:00 +0700

    First commit

---
+++ app/src/main/java/day01/nguyendpt/chidstudy/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package day01.nguyendpt.chidstudy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Intent intent = new Intent(getApplicationContext(),
+                MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
